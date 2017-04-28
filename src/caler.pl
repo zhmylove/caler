b81484d3ba87c36b6c9fd3232e8b0a7799f41df5
focus-on-quality <mt4.ilja@gmail.com>
2017-04-28 19:18:44 +0300

    added put and approx stub

--- src/caler.pl
+++ src/caler.pl
@@ -7,37 +7,36 @@ use warnings;
 no warnings 'experimental';
 use utf8;
 binmode STDOUT, ':utf8';
-
+use Time::Local;
 use CalerDB;
 
 my $DB = CalerDB->new("/tmp/caler.db");
-
 END{ $DB->save_data() };
 
-$DB->put("app1", "CPU", 0, 11);
-$DB->put("app1", "CPU", 1, 22);
-$DB->put("app1", "CPU", 2, 33);
-$DB->put("app1", "CPU", 3, 22);
-$DB->put("app1", "CPU", 4, 44);
-$DB->put("app1", "CPU", 5, 55);
-$DB->put("app1", "CPU", 6, 66);
-$DB->put("app1", "CPU", 7, 77);
-$DB->put("app1", "CPU", 8, 88);
-$DB->put("app1", "CPU", 9, 99);
-$DB->put("app1", "CPU", 10, 100);
-$DB->put("app1", "CPU", 11, 99);
-$DB->put("app1", "CPU", 12, 66);
-$DB->put("app1", "CPU", 13, 35);
-$DB->put("app1", "CPU", 14, 45);
-$DB->put("app1", "CPU", 15, 55);
-$DB->put("app1", "CPU", 16, 34);
-$DB->put("app1", "CPU", 17, 54);
-$DB->put("app1", "CPU", 18, 75);
-$DB->put("app1", "CPU", 19, 85);
-$DB->put("app1", "CPU", 20, 99);
-$DB->put("app1", "CPU", 21, 100);
-$DB->put("app1", "CPU", 22, 75);
-$DB->put("app1", "CPU", 23, 35);
+#$DB->put("app1", "CPU", 0, 11);
+#$DB->put("app1", "CPU", 1, 22);
+#$DB->put("app1", "CPU", 2, 33);
+#$DB->put("app1", "CPU", 3, 22);
+#$DB->put("app1", "CPU", 4, 44);
+#$DB->put("app1", "CPU", 5, 55);
+#$DB->put("app1", "CPU", 6, 66);
+#$DB->put("app1", "CPU", 7, 77);
+#$DB->put("app1", "CPU", 8, 88);
+#$DB->put("app1", "CPU", 9, 99);
+#$DB->put("app1", "CPU", 10, 100);
+#$DB->put("app1", "CPU", 11, 99);
+#$DB->put("app1", "CPU", 12, 66);
+#$DB->put("app1", "CPU", 13, 35);
+#$DB->put("app1", "CPU", 14, 45);
+#$DB->put("app1", "CPU", 15, 55);
+#$DB->put("app1", "CPU", 16, 34);
+#$DB->put("app1", "CPU", 17, 54);
+#$DB->put("app1", "CPU", 18, 75);
+#$DB->put("app1", "CPU", 19, 85);
+#$DB->put("app1", "CPU", 20, 99);
+#$DB->put("app1", "CPU", 21, 100);
+#$DB->put("app1", "CPU", 22, 75);
+#$DB->put("app1", "CPU", 23, 35);
 
 sub approx_app_metric {
    my ($APP, $METRIC) = @_;
@@ -55,8 +54,27 @@ sub approx_app_metric {
 
    $DB->inc_N();
 }
-
-approx_app_metric("app1", "CPU");
-
+sub get_init_offset {
+   my ($sec, $min, $hour) = localtime(time);
+   my $offset = ($sec + $min * 60 + $hour * 3600);
+   return $offset;
+}
+sub gather_data {
+   my $counter = 0;
+   my $step = 60;
+   my $init_offset = get_init_offset();
+   sleep($step - $init_offset % $step);
+   my $border = $init_offset + $step - $init_offset % $step;
+   $counter = $border;
+   for(;;sleep($step)) {
+      $DB->put("app1", "CPU", $counter, int(rand(101)));
+      $counter += $step;
+      if ($counter == $border) {
+          $counter = 0;
+          approx_app_metric("app1", "CPU");
+      }
+   }
+}
+gather_data();
 use Data::Dumper;
 print Dumper($DB->get_DB());
