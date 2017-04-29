21213da09f361fad2ebae5c2ba5eca72abd31ae3
Sergey Zhmylove <zhmylove@narod.ru>
2017-04-29 15:42:28 +0300

    Add SIGHUP handler and remove unnecessary comments

--- src/caler.pl
+++ src/caler.pl
@@ -12,31 +12,7 @@ use CalerDB;
 
 my $DB = CalerDB->new("/tmp/caler.db");
 END{ $DB->save_data() };
-
-#$DB->put("app1", "CPU", 0, 11);
-#$DB->put("app1", "CPU", 1, 22);
-#$DB->put("app1", "CPU", 2, 33);
-#$DB->put("app1", "CPU", 3, 22);
-#$DB->put("app1", "CPU", 4, 44);
-#$DB->put("app1", "CPU", 5, 55);
-#$DB->put("app1", "CPU", 6, 66);
-#$DB->put("app1", "CPU", 7, 77);
-#$DB->put("app1", "CPU", 8, 88);
-#$DB->put("app1", "CPU", 9, 99);
-#$DB->put("app1", "CPU", 10, 100);
-#$DB->put("app1", "CPU", 11, 99);
-#$DB->put("app1", "CPU", 12, 66);
-#$DB->put("app1", "CPU", 13, 35);
-#$DB->put("app1", "CPU", 14, 45);
-#$DB->put("app1", "CPU", 15, 55);
-#$DB->put("app1", "CPU", 16, 34);
-#$DB->put("app1", "CPU", 17, 54);
-#$DB->put("app1", "CPU", 18, 75);
-#$DB->put("app1", "CPU", 19, 85);
-#$DB->put("app1", "CPU", 20, 99);
-#$DB->put("app1", "CPU", 21, 100);
-#$DB->put("app1", "CPU", 22, 75);
-#$DB->put("app1", "CPU", 23, 35);
+$SIG{HUP} = sub { $DB->save_data() };
 
 sub approx_app_metric {
    my ($APP, $METRIC) = @_;
