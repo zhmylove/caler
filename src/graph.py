46115f0c0e46c406b515f8fb3e7654cc6efca09d
Valeriy Kireev <valeriykireev@gmail.com>
2017-11-06 22:44:48 +0300

    Add functions visualizer

---
+++ src/graph.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Made by kk
+
+import os, sys
+
+import matplotlib.pyplot as p
+
+points = []
+for line in sys.stdin:
+    points.append(line.split()), print(*points[-1])
+
+if 0 == os.fork():
+    p.legend(handles=[p.plot(*zip(*points), label='y = f(x)')[0]]), p.show()
