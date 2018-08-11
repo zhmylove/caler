a884f011dd1085c6e4986c34406c63dc6ec2977a
Valeriy Kireev <valeriykireev@gmail.com>
2018-08-12 02:58:34 +0300

    Adapt wgrun.sh for period61.pl

--- src/wgrun.sh
+++ src/wgrun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 WAVEGEN="./wavegen.pl"
-PERIOD="./period.pl"
+PERIOD="./period61.pl"
 PERIODS="2 3 4 5 6 8 10 12"
 
 perl -0l12ne 'print for /#- Generates.*?sub ([^\s(]+)/gs' "$WAVEGEN" |
@@ -9,6 +9,8 @@ while read sub ;do
    for period in $PERIODS ;do
       printf " == %16s(%2d) ==> " "$sub" "$period"
       "$WAVEGEN" "$sub" "-p$period" "-s50" -r0.5 2>/dev/null |
-      "$PERIOD" -nt -f | awk '/Round/{print $3}' # > FILENAME_GOES_HERE
+      cut -d' ' -f2 | sed \$d |
+      "$PERIOD" -nt -f 2>/dev/null |
+      awk '/True period/{print $3/10}' # > FILENAME_GOES_HERE
    done
 done
